f31d79962b4c092638ee1aae82b8b29dbbcf11df
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2018-08-13 08:14:54 -0700

    Agregar archivos de proyecto.

---
+++ PrimerProyecto/Persona.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimerProyecto
+{
+    public class Persona
+    {
+        public int Edad { get; set; }
+        public string Nombre { get; set; }
+
+        //constructor con parametros
+        public Persona(int edad,string nombre)
+        {
+            Edad = edad; // se iguala el parametro a la variable global
+            Nombre = nombre;
+        }
+
+        //constructor sin parametro/vacio
+        public Persona()
+        {
+            Edad = 0; //inicializa la edad en 0
+            Nombre = ""; //inicializa el nombre vacio
+        }
+
+        //crear funcion para que no regrese nada,que solo modifique un atributo
+
+        public void cumplirAnio()
+        {
+            Edad++; //para sumar 1 a la variable global
+        }
+    }
+}
---
+++ PrimerProyecto/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimerProyecto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //crear instancia
+            Persona persona1 = new Persona(25,"José"); //se le pueden poner directamente los valores
+            Console.WriteLine(persona1.Nombre + " tiene " + persona1.Edad + " años.");
+            persona1.cumplirAnio();
+            Console.WriteLine("Ahora " + persona1.Nombre + " tiene " + persona1.Edad + " años. ");
+            Console.ReadLine();
+        }
+    }
+}
